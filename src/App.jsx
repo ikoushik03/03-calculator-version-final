dfe9f7faaff77fe672ca9403454f6a806e0e90f0
koushik Sharma <koushiksharma9939@gmail.com>
2025-04-03 22:28:46 +0530

    change app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import ButtonsContainer from "./components/ButtonsContainer";
 import Display from "./components/display";
-import { useState } from "react";
 
 function App() {
   const [calVal, setCalVal] = useState("");
